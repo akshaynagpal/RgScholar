ad6b9a5a9a2ef826923c3f63a45d67dfc103f077
Akshay Nagpal <akshay2626@gmail.com>
2015-06-18 14:23:56 +0530

    added package files

--- R/RgScholar.R
+++ R/RgScholar.R
@@ -4,7 +4,7 @@ google_Scholar <- function(user_query,year_low=NULL,year_high=NULL,journal=NULL)
   user_query_CLEAN <- gsub("^ *|(?<= ) | *$", "", tolower(user_query), perl=T)  #removing multiple spaces
   user_query_for_URL <- gsub(" ","+",user_query_CLEAN)        #replacing space with + for URL
   base_URL <- "https://scholar.google.co.in/scholar?as_vis=1&hl=en&q="
-  query_URL <- paste(base_url,user_query_for_url,sep="")
+  query_URL <- paste(base_URL,user_query_for_URL,sep="")
   
   if(!is.null(journal)){
     journal_CLEAN <- gsub("^ *|(?<= ) | *$", "", tolower(journal), perl=T)  #removing multiple spaces
@@ -13,30 +13,34 @@ google_Scholar <- function(user_query,year_low=NULL,year_high=NULL,journal=NULL)
   }
   
   if(!is.null(year_low)){
-    query_URL <- paste(query_URL,"&as_ylo=",year_low)
+    query_URL <- paste(query_URL,"&as_ylo=",year_low,sep = "")
   }
   
   if(!is.null(year_high)){
-    query_URL <- paste(query_URL,"&as_yhi=",year_high)
+    query_URL <- paste(query_URL,"&as_yhi=",year_high,sep="")
   }
+  print(query_URL)
+  HTML_from_query_URL <- read_html(query_URL)
+  
+  getTitles<- HTML_from_query_URL %>%
+    html_nodes(".gs_rt") %>%
+    html_text()
+  
+  getLinks<- HTML_from_query_URL %>%
+    html_nodes(".gs_rt a") %>%
+    html_attr("href")
+  
+  getShortAbstract<- HTML_from_query_URL %>%
+    html_nodes(".gs_rs") %>%
+    html_text()
+  result <- data.frame(title=getTitles,link=getLinks,ShortAbstract=getShortAbstract)
+  
+  return(result)
 } 
 
 
 
 
-p1 <- read_html('https://scholar.google.co.in/scholar?as_vis=1&hl=en&q=heart+rate+variability+analysis')
-
-getTitles<- p1 %>%
-  html_nodes(".gs_rt") %>%
-  html_text()
-
-getLinks<- p1 %>%
-  html_nodes(".gs_rt a") %>%
-  html_attr("href")
-
-getShortAbstract<- p1 %>%
-  html_nodes(".gs_rs") %>%
-  html_text()
 
 
-result <- data.frame(title=getTitles,link=getLinks,ShortAbstract=getShortAbstract)
+

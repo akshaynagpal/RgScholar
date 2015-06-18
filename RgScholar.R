8d3d7f9302e4df9fe79ec704d85f930d7e4e0963
Akshay Nagpal <akshay2626@gmail.com>
2015-06-18 13:22:42 +0530

    implemented search query formation based on parameters given by the user

--- RgScholar.R
+++ RgScholar.R
@@ -1,5 +1,29 @@
 library(rvest)
 
+google_Scholar <- function(user_query,year_low=NULL,year_high=NULL,journal=NULL){
+  user_query_CLEAN <- gsub("^ *|(?<= ) | *$", "", tolower(user_query), perl=T)  #removing multiple spaces
+  user_query_for_URL <- gsub(" ","+",user_query_CLEAN)        #replacing space with + for URL
+  base_URL <- "https://scholar.google.co.in/scholar?as_vis=1&hl=en&q="
+  query_URL <- paste(base_url,user_query_for_url,sep="")
+  
+  if(!is.null(journal)){
+    journal_CLEAN <- gsub("^ *|(?<= ) | *$", "", tolower(journal), perl=T)  #removing multiple spaces
+    journal_for_URL <- gsub(" ","+",journal_CLEAN)        #replacing space with + for URL
+    query_URL <- paste(query_URL,"&as_publication=",journal_for_URL,sep="")
+  }
+  
+  if(!is.null(year_low)){
+    query_URL <- paste(query_URL,"&as_ylo=",year_low)
+  }
+  
+  if(!is.null(year_high)){
+    query_URL <- paste(query_URL,"&as_yhi=",year_high)
+  }
+} 
+
+
+
+
 p1 <- read_html('https://scholar.google.co.in/scholar?as_vis=1&hl=en&q=heart+rate+variability+analysis')
 
 getTitles<- p1 %>%
